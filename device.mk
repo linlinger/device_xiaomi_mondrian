fd1baedcffde6c1c775bf64a7ccd107bbd89740e
sea.deep <sea.deep.deep@gmail.com>
2024-02-09 02:35:04 -0500

    mondrian: configs: Add display configs

--- device.mk
+++ device.mk
@@ -15,6 +15,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/resourcemanager_waipio_mtp.xml
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfigs/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055170.xml \
+    $(LOCAL_PATH)/configs/displayconfigs/resolution_switch_process_list_backup.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/resolution_switch_process_list_backup.xml \
+    $(LOCAL_PATH)/configs/displayconfigs/thermal_brightness_control.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/thermal_brightness_control.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMondrian \
